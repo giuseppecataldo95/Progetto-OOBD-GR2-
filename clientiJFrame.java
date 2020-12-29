fde580845ff0c61e346b1abeefc752e6d10ba425
giuseppecataldo95 <75066437+giuseppecataldo95@users.noreply.github.com>
2020-12-29 19:27:03 +0100

    Add files via upload
    Creazione finestra creaTessera, erroreTessera e clienti.

---
+++ clientiJFrame.java
@@ -0,0 +1,111 @@
+package Cliente;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JSeparator;
+import java.awt.Dimension;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+
+public class clientiJFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					clientiJFrame frame = new clientiJFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public clientiJFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(150, 80, 1000, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JToolBar toolBar = new JToolBar();
+		toolBar.setBorder(null);
+		toolBar.setAlignmentX(Component.LEFT_ALIGNMENT);
+		toolBar.setBorderPainted(false);
+		toolBar.setFloatable(false);
+		toolBar.setBackground(Color.WHITE);
+		toolBar.setMaximumSize(new Dimension(100, 100));
+		toolBar.setBounds(0, 11, 65, 563);
+		toolBar.setOrientation(SwingConstants.VERTICAL);
+		contentPane.add(toolBar);
+		
+		JButton clienteButton = new JButton("Clienti");
+		clienteButton.setForeground(Color.BLACK);
+		toolBar.add(clienteButton);
+		clienteButton.setIcon(null);
+		clienteButton.setMaximumSize(new Dimension(65, 70));
+		
+		JButton venditeButton = new JButton("Vendite");
+		venditeButton.setBorder(null);
+		venditeButton.setBorderPainted(false);
+		venditeButton.setIcon(new ImageIcon("C:\\Users\\enzos\\Desktop\\Progetto\\cassaaaa.png"));
+		venditeButton.setMaximumSize(new Dimension(65, 70));
+		toolBar.add(venditeButton);
+		
+		JButton dipendenteButton = new JButton("Dipendenti");
+		dipendenteButton.setIcon(new ImageIcon("C:\\Users\\enzos\\Desktop\\Progetto\\scatolaaaa.png"));
+		dipendenteButton.setBorderPainted(false);
+		dipendenteButton.setBorder(null);
+		dipendenteButton.setMaximumSize(new Dimension(65, 70));
+		toolBar.add(dipendenteButton);
+		
+		JButton magazzinoButton = new JButton("Magazzino");
+		magazzinoButton.setIcon(new ImageIcon("C:\\Users\\enzos\\Desktop\\Progetto\\dipendentee.png"));
+		magazzinoButton.setBorderPainted(false);
+		magazzinoButton.setBorder(null);
+		magazzinoButton.setBackground(UIManager.getColor("ToolBar.dockingBackground"));
+		magazzinoButton.setMaximumSize(new Dimension(65, 70));
+		toolBar.add(magazzinoButton);
+		
+		Component verticalStrut = Box.createVerticalStrut(200);
+		verticalStrut.setBackground(Color.LIGHT_GRAY);
+		toolBar.add(verticalStrut);
+		
+		JButton infoButton = new JButton("Info");
+		infoButton.setIcon(new ImageIcon("C:\\Users\\enzos\\Desktop\\Progetto\\infoo.png"));
+		infoButton.setBorder(null);
+		infoButton.setBorderPainted(false);
+		infoButton.setMaximumSize(new Dimension(65, 70));
+		toolBar.add(infoButton);
+		
+		JButton creaTesseraButton = new JButton("Crea Nuova Tessera");
+		creaTesseraButton.setFont(new Font("Arial", Font.PLAIN, 14));
+		creaTesseraButton.setBounds(315, 228, 183, 102);
+		contentPane.add(creaTesseraButton);
+		
+		JButton visualizzaClientiButton = new JButton("Visualizza Clienti");
+		visualizzaClientiButton.setFont(new Font("Arial", Font.PLAIN, 14));
+		visualizzaClientiButton.setBounds(534, 228, 183, 102);
+		contentPane.add(visualizzaClientiButton);
+	}
+}
