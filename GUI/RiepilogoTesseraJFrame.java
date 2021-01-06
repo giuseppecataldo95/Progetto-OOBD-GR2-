adde200599038fe6292398910670b6d8e18b1e57
Giovanni <Giovanni@192.168.0.175>
2021-01-06 10:10:02 +0100

    Aggiunta gui Dipendenti

--- GUI/RiepilogoTesseraJFrame.java
+++ GUI/RiepilogoTesseraJFrame.java
@@ -37,6 +37,7 @@ public class RiepilogoTesseraJFrame extends JFrame {
 	 JTextField RiepilogoCFTB;
 	 JTextField RiepilogoMeseNTB;
 	 public JTextField RiepilogoAnnoNTB;
+	 private JTextField textField;
 
 	
 	
@@ -48,7 +49,7 @@ public class RiepilogoTesseraJFrame extends JFrame {
 		setBounds(150, 80, 1000, 600);
 
 		RiepilogoTesseraPanel = new JPanel();
-		RiepilogoTesseraPanel.setBackground(new Color(255, 228, 181));
+		RiepilogoTesseraPanel.setBackground(new Color(0, 204, 102));
 		RiepilogoTesseraPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		RiepilogoTesseraPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(RiepilogoTesseraPanel);
@@ -131,25 +132,25 @@ public class RiepilogoTesseraJFrame extends JFrame {
 		PercorsoTB.setBounds(65, 0, 976, 30);
 		RiepilogoTesseraPanel.add(PercorsoTB);
 
-		JButton ClientiPercorsoButton = new JButton("> Clienti");
+		JButton ClientiPercorsoButton = new JButton("> Dipendenti");
 		ClientiPercorsoButton.setFont(new Font("Arial", Font.PLAIN, 11));
 		PercorsoTB.add(ClientiPercorsoButton);
 		
-		JButton CreaNuovaTesseraPercorsoButton = new JButton("> Nuova Tessera");
+		JButton CreaNuovaTesseraPercorsoButton = new JButton("> Nuova Dipendente");
 		CreaNuovaTesseraPercorsoButton.setFont(new Font("Arial", Font.PLAIN, 11));
 		PercorsoTB.add(CreaNuovaTesseraPercorsoButton);
 		
-		JButton RiepilogoNuovaTesseraPercorsoButton = new JButton("> Riepilogo Nuova Tessera");
+		JButton RiepilogoNuovaTesseraPercorsoButton = new JButton("> Riepilogo Nuovo Dipendente\r\n");
 		RiepilogoNuovaTesseraPercorsoButton.setFont(new Font("Arial", Font.PLAIN, 11));
 		PercorsoTB.add(RiepilogoNuovaTesseraPercorsoButton);
 		
-		JLabel Riepilogo1LB = new JLabel("Questo \u00E8 il riepilogo delle informazione del Cliente!");
+		JLabel Riepilogo1LB = new JLabel("Questo \u00E8 il riepilogo delle informazione del Dipendente\r\n");
 		Riepilogo1LB.setHorizontalAlignment(SwingConstants.CENTER);
 		Riepilogo1LB.setFont(new Font("Arial", Font.BOLD, 14));
 		Riepilogo1LB.setBounds(279, 54, 416, 48);
 		RiepilogoTesseraPanel.add(Riepilogo1LB);
 		
-		JLabel Riepilogo2LB = new JLabel("Controlla attentamente i dati inseriti!");
+		JLabel Riepilogo2LB = new JLabel("Controlla attentamente i dati inseriti!\r\n\r\n");
 		Riepilogo2LB.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		Riepilogo2LB.setBounds(372, 112, 268, 13);
 		RiepilogoTesseraPanel.add(Riepilogo2LB);
@@ -157,38 +158,38 @@ public class RiepilogoTesseraJFrame extends JFrame {
 		JLabel NomeInseritoLB = new JLabel("Nome Inserito:  ");
 		NomeInseritoLB.setHorizontalAlignment(SwingConstants.RIGHT);
 		NomeInseritoLB.setFont(new Font("Arial", Font.PLAIN, 12));
-		NomeInseritoLB.setBounds(173, 181, 176, 13);
+		NomeInseritoLB.setBounds(173, 152, 176, 13);
 		RiepilogoTesseraPanel.add(NomeInseritoLB);
 		
 		JLabel CognomeInseritoLB = new JLabel("Cognome Inserito:  ");
 		CognomeInseritoLB.setHorizontalAlignment(SwingConstants.RIGHT);
 		CognomeInseritoLB.setFont(new Font("Arial", Font.PLAIN, 12));
-		CognomeInseritoLB.setBounds(173, 236, 176, 13);
+		CognomeInseritoLB.setBounds(173, 197, 176, 13);
 		RiepilogoTesseraPanel.add(CognomeInseritoLB);
 		
 		JLabel SessoInseritoLB = new JLabel("Sesso Inserito:  ");
 		SessoInseritoLB.setHorizontalAlignment(SwingConstants.RIGHT);
 		SessoInseritoLB.setFont(new Font("Arial", Font.PLAIN, 12));
-		SessoInseritoLB.setBounds(173, 291, 176, 13);
+		SessoInseritoLB.setBounds(173, 245, 176, 13);
 		RiepilogoTesseraPanel.add(SessoInseritoLB);
 		
 		JLabel LuogoNInseritoLB = new JLabel("Luogo di nascita Inserito:   ");
 		LuogoNInseritoLB.setHorizontalAlignment(SwingConstants.RIGHT);
 		LuogoNInseritoLB.setFont(new Font("Arial", Font.PLAIN, 12));
-		LuogoNInseritoLB.setBounds(173, 347, 176, 13);
+		LuogoNInseritoLB.setBounds(173, 297, 176, 13);
 		RiepilogoTesseraPanel.add(LuogoNInseritoLB);
 		
 		JLabel DataInseritaLB = new JLabel("Data di nascita Inserita:   ");
 		DataInseritaLB.setHorizontalAlignment(SwingConstants.RIGHT);
 		DataInseritaLB.setFont(new Font("Arial", Font.PLAIN, 12));
-		DataInseritaLB.setBounds(173, 393, 176, 13);
+		DataInseritaLB.setBounds(173, 347, 176, 13);
 		RiepilogoTesseraPanel.add(DataInseritaLB);
 
 		
 		JLabel CodFiscaleLB = new JLabel("Codice Fiscale Calcolato: ");
 		CodFiscaleLB.setHorizontalAlignment(SwingConstants.RIGHT);
 		CodFiscaleLB.setFont(new Font("Arial", Font.PLAIN, 12));
-		CodFiscaleLB.setBounds(173, 442, 176, 13);
+		CodFiscaleLB.setBounds(173, 388, 176, 13);
 
 		RiepilogoTesseraPanel.add(CodFiscaleLB);
 		
@@ -202,58 +203,70 @@ public class RiepilogoTesseraJFrame extends JFrame {
 		
 		RiepilogoNomeTB = new JTextField();
 		RiepilogoNomeTB.setEditable(false);
-		RiepilogoNomeTB.setBounds(399, 178, 215, 19);
+		RiepilogoNomeTB.setBounds(399, 148, 215, 19);
 		RiepilogoTesseraPanel.add(RiepilogoNomeTB);
 		RiepilogoNomeTB.setColumns(10);
 		
 		RiepilogoCognomeTB = new JTextField();
 		RiepilogoCognomeTB.setEditable(false);
 		RiepilogoCognomeTB.setColumns(10);
-		RiepilogoCognomeTB.setBounds(399, 233, 215, 19);
+		RiepilogoCognomeTB.setBounds(399, 193, 215, 19);
 		RiepilogoTesseraPanel.add(RiepilogoCognomeTB);
 		
 		RiepilogoSessoTB = new JTextField();
 		RiepilogoSessoTB.setEditable(false);
 		RiepilogoSessoTB.setColumns(10);
-		RiepilogoSessoTB.setBounds(399, 288, 215, 19);
+		RiepilogoSessoTB.setBounds(399, 241, 215, 19);
 		RiepilogoTesseraPanel.add(RiepilogoSessoTB);
 		
 		RiepilogoLuogoNTB = new JTextField();
 		RiepilogoLuogoNTB.setEditable(false);
 		RiepilogoLuogoNTB.setColumns(10);
-		RiepilogoLuogoNTB.setBounds(399, 344, 215, 19);
+		RiepilogoLuogoNTB.setBounds(399, 293, 215, 19);
 		RiepilogoTesseraPanel.add(RiepilogoLuogoNTB);
 		
 		RiepilogoGiornoNTB = new JTextField();
 		RiepilogoGiornoNTB.setEditable(false);
 		RiepilogoGiornoNTB.setColumns(10);
-		RiepilogoGiornoNTB.setBounds(399, 390, 35, 19);
+		RiepilogoGiornoNTB.setBounds(399, 343, 35, 19);
 		RiepilogoTesseraPanel.add(RiepilogoGiornoNTB);
 		
 		RiepilogoCFTB = new JTextField();
 		RiepilogoCFTB.setEditable(false);
 		RiepilogoCFTB.setColumns(10);
-		RiepilogoCFTB.setBounds(399, 439, 215, 19);
+		RiepilogoCFTB.setBounds(399, 384, 215, 19);
 		RiepilogoTesseraPanel.add(RiepilogoCFTB);
 		
 		RiepilogoMeseNTB = new JTextField();
 		RiepilogoMeseNTB.setEditable(false);
 		RiepilogoMeseNTB.setColumns(10);
-		RiepilogoMeseNTB.setBounds(454, 390, 107, 19);
+		RiepilogoMeseNTB.setBounds(444, 343, 107, 19);
 		RiepilogoTesseraPanel.add(RiepilogoMeseNTB);
 		
 		RiepilogoAnnoNTB = new JTextField();
 		RiepilogoAnnoNTB.setEditable(false);
 		RiepilogoAnnoNTB.setColumns(10);
-		RiepilogoAnnoNTB.setBounds(581, 390, 65, 19);
+		RiepilogoAnnoNTB.setBounds(561, 343, 65, 19);
 		RiepilogoTesseraPanel.add(RiepilogoAnnoNTB);
 		
 		JLabel SlashData1 = new JLabel("/");
-		SlashData1.setBounds(444, 393, 28, 13);
+		SlashData1.setBounds(435, 347, 28, 13);
 		RiepilogoTesseraPanel.add(SlashData1);
 		
 		JLabel SlashData2 = new JLabel("/");
-		SlashData2.setBounds(571, 393, 28, 13);
+		SlashData2.setBounds(550, 347, 28, 13);
 		RiepilogoTesseraPanel.add(SlashData2);
+		
+		JLabel lblIdUnivocoInserito = new JLabel("ID Univoco Inserito:");
+		lblIdUnivocoInserito.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblIdUnivocoInserito.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblIdUnivocoInserito.setBounds(173, 420, 176, 13);
+		RiepilogoTesseraPanel.add(lblIdUnivocoInserito);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setColumns(10);
+		textField.setBounds(399, 414, 45, 19);
+		RiepilogoTesseraPanel.add(textField);
 	}
 }
