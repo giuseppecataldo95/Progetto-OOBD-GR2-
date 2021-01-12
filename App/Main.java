57bc1d97b775213b6ebec138353d7b803e3826f9
giuseppecataldo95 <75066437+giuseppecataldo95@users.noreply.github.com>
2021-01-12 22:37:51 +0100

    Modifiche

--- App/Main.java
+++ App/Main.java
@@ -10,57 +10,49 @@ import DAO.ClienteDAO;
 import DAO.MagazzinoDAO;
 import Entità.Cliente;
 import Entità.Frutta;
-import GUI.FinestraPrincipaleJFrame;
-import GUI.Cliente.ClientiJFrame;
-import GUI.Cliente.CreaTesseraJFrame;
+import GUI.ClientiJFrame;
+import GUI.CreaTesseraJFrame;
 import ImplementazioniDAO.ClienteDAOPostgres;
 import ImplementazioniDAO.MagazzinoDAOPostgres;
 
 public class Main {
 
-
+	
 
 	public static void main(String[] args) {
-
-
+		
+		
 		ConnessioneDB DBconn = null;
 		Connection Conn = null;
 		ClienteDAO cliente = null;
-//		FinestraPrincipaleJFrame f = new FinestraPrincipaleJFrame();
-//		f.setVisible(true);
-//	
-
-
+		
+		
 		try {
 			DBconn = ConnessioneDB.getInstance();
 			Conn = DBconn.getConnection();
 
-			
 
-
-			ControllerMagazzino Magazzino = new ControllerMagazzino(Conn);
-//			ControllerCliente Cliente = new ControllerCliente(Conn);
-
-
-
-
+			//ControllerMagazzino Magazzino = new ControllerMagazzino(Conn);
+			ControllerCliente Cliente = new ControllerCliente(Conn);
+ 
+		
 		}catch(SQLException e) {
 			System.out.println("Errore: "+e);
 		}
-
-
-
-
+	
+		
+		
+	
 	}
-
-
-
-
-
-
-
-
-
+	
+		
+		
+		
+		
+		
+		
+		
+	
 
 
 }
